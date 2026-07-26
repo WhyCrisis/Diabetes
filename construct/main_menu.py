3a0f61702638d564f43371dba42bf9b6ee15070f
WhyCrisis <zertoswer@gmail.com>
2026-07-26 19:09:59 +0300

    New menu that goes after start and verification

---
+++ construct/main_menu.py
@@ -0,0 +1,19 @@
+#----
+from aiogram import Router, F
+from aiogram.enums import ParseMode
+from aiogram.filters import Command
+from aiogram.types import (Message,ReplyKeyboardMarkup,
+                           KeyboardButton,
+                           InlineKeyboardButton,
+                           InlineKeyboardMarkup,
+                           CallbackQuery,
+                           FSInputFile
+                           )
+from aiogram.types import ReplyKeyboardRemove
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import json
+#----
+router = Router()
+#----
+
+
