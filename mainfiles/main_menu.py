0b8e4e73dd7a80452a07c366bf06d2f66b95c0cb
WhyCrisis <zertoswer@gmail.com>
2026-07-31 21:46:57 +0300

    Во избежание повторых записей в БД при изменении языка решено было вернуть состояние ФСМ для удобства сохранения и передачи уже ГОТОВЫХ и ТОЧНЫХ данных в БД

--- mainfiles/main_menu.py
+++ mainfiles/main_menu.py
@@ -12,8 +12,27 @@ from aiogram.types import (Message,ReplyKeyboardMarkup,
 from aiogram.types import ReplyKeyboardRemove
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import json
+
+#----
+#Databases
+from databases.database_SQlite import get_user_language
+#----
 #----
 router = Router()
 #----
+
+@router.callback_query(F.data.startswith('agree_'))
+async def main_menu(callback: CallbackQuery):
+    #хватаем айди из кнопки
+    user_id = callback.from_user.id
+    user_lang = get_user_language(user_id)
+    print(user_lang)
+
+
+
+
+
+
+
 
 
