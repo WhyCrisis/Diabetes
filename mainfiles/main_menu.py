f3d2361ccbe6025e307b2ca6cb4158f91a5e215e
WhyCrisis <zertoswer@gmail.com>
2026-08-06 12:16:00 +0300

    clear. No need of that func

--- mainfiles/main_menu.py
+++ mainfiles/main_menu.py
@@ -15,18 +15,11 @@ import json
 
 #----
 #Databases
-from databases.database_SQlite import get_user_language
+#from databases.database_SQlite import get_user_language
 #----
 #----
 router = Router()
 #----
-
-@router.callback_query(F.data.startswith('agree_'))
-async def main_menu(callback: CallbackQuery):
-    #хватаем айди из кнопки
-    user_id = callback.from_user.id
-    user_lang = get_user_language(user_id)
-    print(user_lang)
 
 
 
