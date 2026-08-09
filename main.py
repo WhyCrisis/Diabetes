1577245bdb490c80feea2717c88f2e33f8e3e63a
WhyCrisis <zertoswer@gmail.com>
2026-08-09 18:00:17 +0300

    Убрана рассылка так как не является нужной. Подчисщены библиотеки

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 
-from dbm import sqlite3
+import sqlite3
 from os import getenv
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.client import bot
 from dotenv import load_dotenv
-import aiosqlite
 #---
 from mainfiles.afterstartcommand import router as starter_router
 from construct.keyboards import router as constructor_router
 from databases.database_SQlite import router as database_router
 from mainfiles.main_menu import router as menu_router
-#---
-from databases.database_SQlite import log_start
 #---
 load_dotenv()
 TOKEN = getenv("BOT_TOKEN")
@@ -28,38 +24,10 @@ dp.include_router(database_router)
 dp.include_router(menu_router)
 #---
 
-
-#---рассылка
-async def if_restart_db():
-    await log_start()
-    try:
-        async with aiosqlite.connect(DB_name) as db:
-            cursor = await db.execute("SELECT id_user FROM users ")
-            users = await cursor.fetchall()
-            return users
-    except sqlite3.OperationalError as e:
-            print(f'Ошибка при подключении к базе данных: {e}')
-            return []
-
-async def sending(bot: Bot):
-    users = await if_restart_db()
-    for user in users:
-        user_id = user[0]
-        try:
-            await bot.send_message(user_id, "Restart needed! Please use /restart command to restart")
-            await asyncio.sleep(1)
-        except Exception as e:
-            print(f'Не удалось отправить сообщение некоторым пользователям: {user_id}')
-#---
-
 #---инициализация
 async def main():
     bot = Bot(token=TOKEN)
     print("Запущено")
-
-    await if_restart_db()
-    await sending(bot)
-
     await dp.start_polling(bot)
 #---
 if __name__ == '__main__':
