71c49a319ae8b552c87291aec1c8de546f629b81
WhyCrisis <zertoswer@gmail.com>
2026-09-03 22:14:21 +0300

    Перевод на английский, логика удаления с двухэтапной верефикацией и возврат на админское меню

--- mainfiles/admin_commands.py
+++ mainfiles/admin_commands.py
@@ -1,4 +1,5 @@
 #----
+import asyncio
 from os import getenv
 from aiogram import Router, F, Bot
 from aiogram.enums import parse_mode
@@ -13,7 +14,7 @@ print(auid)
 router = Router()
 #----
 #Databases
-from databases.database_SQlite import get_user_anket, delete_user, get_stats_last_join, get_stats_user_language, get_stats_user_count
+from databases.database_SQlite import get_user_anket, delete_user, get_stats_last_join, get_stats_user_language, get_stats_user_count, log_start
 
 #----
 #Keyboards
@@ -37,10 +38,13 @@ async def help(message: Message):
 
 @router.callback_query(F.data == 'drop')
 async def asdads(callback: CallbackQuery):
-    text = ('This command is <b>PERMANENT</b>!/n That means that no undo of that!/n To proceed push button below')
+    text = ('This command is <b>PERMANENT</b>!\nThat means that no undo of that!\nTo proceed push button below')
     parse_mode = 'HTML'
+    await callback.message.delete()
     await callback.answer()
-    await message.answer(text,parse_mode=parse_mode,reply_markup=delete_db())
+    await callback.message.answer(text,parse_mode=parse_mode,reply_markup=delete_db())
+
+
 
 
 @router.message(Command('admin_menu_with_things'))
@@ -49,15 +53,35 @@ async def admin_menu_with_things(message: Message):
         users = await get_stats_user_count()
         top = await get_stats_user_language()
         last = await get_stats_last_join()
+
         text =(f''
                f'Diabetes bot\n'
                f'-------------------------\n\n\n'
-               f'Привет. Общая сводка:\n'
-               f'Количество пользователей: <b>{users}</b>\n'
-               f'Популярный язык: <b>{top}</b>\n'
-               f'Последняя регистрация: <b>{last} GMC +3</b>\n')
+               f'Hi, total count:\n'
+               f'Users: <b>{users}</b>\n'
+               f'Popular language: <b>{top}</b>\n'
+               f'Last registration: <b>{last} GMC +3</b>\n')
         parse_mode = 'HTML'
         reply_markup = fast_admin_things()
         await message.answer(text,parse_mode=parse_mode,reply_markup=reply_markup)
     else:
         message.answer('Denied')
+
+@router.callback_query(F.data == 'drop_admin')
+async def drop_admin(callback: CallbackQuery):
+    await callback.answer('Deleted! This action is in log now!', show_alert=True)
+    await callback.answer()
+
+    #Удаление и создание новой базы
+    await delete_user()
+    #Создание
+    await log_start()
+
+    await callback.message.delete()
+    await admin_menu_with_things(callback.message)
+
+@router.callback_query(F.data == 'back_to_admin')
+async def back_admin(callback: CallbackQuery):
+    await callback.answer()
+    await admin_menu_with_things(callback.message)
+    await callback.message.delete()
