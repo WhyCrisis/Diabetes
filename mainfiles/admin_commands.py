84a49336842b9d83f88838a8a89da2a097bcaa8f
WhyCrisis <zertoswer@gmail.com>
2026-09-04 20:58:38 +0300

    Подключение при запуске админской команды и добавление записи в БД

--- mainfiles/admin_commands.py
+++ mainfiles/admin_commands.py
@@ -14,7 +14,8 @@ print(auid)
 router = Router()
 #----
 #Databases
-from databases.database_SQlite import get_user_anket, delete_user, get_stats_last_join, get_stats_user_language, get_stats_user_count, log_start
+from databases.database_SQlite import get_user_anket, delete_user, get_stats_last_join, get_stats_user_language, \
+    get_stats_user_count, log_start, log_admin, do_admin, see_admin
 
 #----
 #Keyboards
@@ -35,6 +36,16 @@ async def help(message: Message):
             text += f"Айди: {user[0]} | Язык: {user[1]} | Штамп: {user[2]}\n"
         await message.answer(text,reply_markup=hard_reset())
 
+@router.message(Command('alo_admins'))
+async def admins_fast_check(message: Message):
+        users = await see_admin()
+        if not users:
+            await message.answer('База пустая')
+            return
+        text = 'Текущие пользователи:\n\n'
+        for user in users:
+            text += f"Айди: {user[0]} | Действие: {user[1]} | Штамп: {user[2]}\n"
+        await message.answer(text)
 
 @router.callback_query(F.data == 'drop')
 async def asdads(callback: CallbackQuery):
@@ -50,6 +61,7 @@ async def asdads(callback: CallbackQuery):
 @router.message(Command('admin_menu_with_things'))
 async def admin_menu_with_things(message: Message):
     if F.from_user.id == auid:
+        await log_admin()
         users = await get_stats_user_count()
         top = await get_stats_user_language()
         last = await get_stats_last_join()
@@ -69,6 +81,14 @@ async def admin_menu_with_things(message: Message):
 
 @router.callback_query(F.data == 'drop_admin')
 async def drop_admin(callback: CallbackQuery):
+
+    #---Логирование---
+    id_user = callback.from_user.id
+    action = 'Database drop'
+    await do_admin(id_user, action)
+    print(action)
+    #---Логирование---
+
     await callback.answer('Deleted! This action is in log now!', show_alert=True)
     await callback.answer()
 
