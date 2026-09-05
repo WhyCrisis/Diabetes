190758e2e1b19fcd455e2a156413c7ed0584eb17
WhyCrisis <zertoswer@gmail.com>
2026-09-05 23:18:21 +0300

    Новая конструкция для бан панели

--- mainfiles/FSM.py
+++ mainfiles/FSM.py
@@ -3,4 +3,10 @@ from aiogram.fsm.state import StatesGroup, State
 class Form(StatesGroup):
     lang = State()
 
+class Ban_form(StatesGroup):
+    id_admin = State()
+    id_user = State()
+    reason = State()
+
+
 
