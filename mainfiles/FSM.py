518916e01efe5cf1fdfb7de5ef7c212748d416d9
WhyCrisis <zertoswer@gmail.com>
2026-09-06 01:40:36 +0300

    Удаление функции бана

--- mainfiles/FSM.py
+++ mainfiles/FSM.py
@@ -3,10 +3,3 @@ from aiogram.fsm.state import StatesGroup, State
 class Form(StatesGroup):
     lang = State()
 
-class Ban_form(StatesGroup):
-    id_admin = State()
-    id_user = State()
-    reason = State()
-
-class UnBan_form(StatesGroup):
-    id_user_unban = State()
