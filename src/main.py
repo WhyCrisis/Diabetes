0f6047085aef694cd3446ed37f1302c0f2c914c2
WhyCrisis <zertoswer@gmail.com>
2026-09-12 12:04:36 +0300

    Разделение логик управления для более точной авторизации

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from os import getenv
 import asyncio
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
+
 #---
 from src.Admin.admin_menu import router as admin_router
 from src.Start.start_menu import router as start_router
@@ -9,15 +10,17 @@ from src.Menu.menu_main import router as menu_router
 
 #---Инициализация БД
 from src.Start.start_SQL import log_start
-#---
+
 load_dotenv()
 TOKEN = getenv("BOT_TOKEN")
 DB_name = "DB_start"
 #---
 
-#---
-#подключение новых роутеров
+#---Инициализация бота и диспетчера
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+#подключение новых роутеров
 dp.include_router(admin_router)
 dp.include_router(start_router)
 dp.include_router(menu_router)
@@ -25,9 +28,8 @@ dp.include_router(menu_router)
 
 #---инициализация
 async def main():
-    bot = Bot(token=TOKEN)
     print("Запущено")
-    await log_start()
+    log_start()
     await dp.start_polling(bot)
 
 #---
